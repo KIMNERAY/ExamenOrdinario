510642777fda069691f64313a93d92bfae0db063
KIMNERAY <garciapinachogael@gmail.com>
2025-06-24 12:25:17 -0600

    añadi el proyecto de vue

---
+++ Ordinario_Ejercicio1/ejercicio1_copiaexamen/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import PantallaContador from "./components/PantallaContador.vue";
+</script>
+
+<template>
+  <PantallaContador/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ Ordinario_Ejercicio1/ejercicio1_copiaexamen/src/components/PantallaContador.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+  <h1>Contador: {{ count }}</h1>
+
+  <div class="Button">
+    <button type="button" @click="count++"> Incremento </button>
+  </div>
+
+  <div class="Button">
+    <button type="button" @click="count--">Decremento</button>
+  </div>
+
+</template>
+
+<style scoped>
+.Button {
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
